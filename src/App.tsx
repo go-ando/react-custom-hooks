7dc0bf9e779a16caf929185eebf18a460c48a51d
go-ando <52556625+go-ando@users.noreply.github.com>
2021-03-23 12:11:20 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { UserCard } from "./compnents/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
+import "./styles.css";
+
+export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const ocClickFetchUser = () => getUsers();
+  return (
+    <div className="App">
+      <button onClick={ocClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
